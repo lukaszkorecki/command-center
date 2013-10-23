812d1db8dbf39bcc28ab1cd308af35618322617d
Łukasz Korecki <lukasz@coffeesounds.com>
2013-10-23 23:43:52 +0100

    Lang customizations + better dependencies.el

--- dependencies.el
+++ dependencies.el
@@ -1,20 +1,16 @@
 (load-file "package.el")
 (package-list-packages)
-(package-install 'color-theme-solarized)
-(package-install 'color-theme)
-(package-install 'evil)
-(package-install 'evil-indent-textobject)
-(package-install 'evil-leader)
-(package-install 'evil-matchit)
-(package-install 'evil-numbers)
-(package-install 'gitignore-mode)
-(package-install 'go-mode)
-(package-install 'handlebars-mode)
-(package-install 'hlinum)
-(package-install 'js2-mode)
-(package-install 'magit)
-(package-install 'makefile-runner)
-(package-install 'markdown-mode)
-(package-install 'rspec-mode)
-(package-install 'ruby-mode)
-(package-install 'undo-tree)
+(defvar lk-packages
+  '(color-theme-solarized color-theme
+		evil evil-indent-textobject evil-leader evil-matchit evil-numbers
+    gitignore-mode
+		go-mode
+		handlebars-mode
+		markdown-mode
+		rspec-mode ruby-mode
+		js2-mode
+		magit
+    makefile-runner  undo-tree))
+
+(loop for p in lk-packages
+      when (not (package-installed-p p)) do (package-install p))
