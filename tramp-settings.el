4e97579775ed5971c5c7bcf6632b59ed8ef15fd5
Łukasz Korecki <lukasz@coffeesounds.com>
2014-02-24 22:17:17 +0000

    Split configs into files

--- tramp-settings.el
+++ tramp-settings.el
@@ -3,8 +3,7 @@
 (setq tramp-default-method "ssh")
 
 ; functions for quickly browsing Vagrant vms
-
 (defun nav-dev-machine ()
   "Navigate to vagrant VM"
   (interactive)
-  (find-file-other-window "/ssh/vagrant@192.168.33.11:/home/vagrant/"))
+  (find-file-other-window "/vagrant@192.168.33.11:/home/vagrant"))
