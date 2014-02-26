1c58e6b57c1975dfde2d00871e320fa755276810
Łukasz Korecki <lukasz@coffeesounds.com>
2014-02-26 22:23:35 +0000

    Remove color overrides

--- init.el
+++ init.el
@@ -66,11 +66,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(background-color "#7f7f7f")
  '(background-mode dark)
- '(cursor-color "#5c5cff")
  '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(foreground-color "#5c5cff")
  '(js2-basic-offset 2)
  '(js2-bounce-indent-p t))
 (custom-set-faces
@@ -78,5 +75,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "terminus"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "terminus"))))
  '(fixed-pitch ((t (:family "terminus")))))
--- settings/remote.el
+++
@@ -1,9 +0,0 @@
-; tramp configuration
-(require 'tramp)
-(setq tramp-default-method "ssh")
-
-; functions for quickly browsing Vagrant vms
-(defun nav-dev-machine ()
-  "Navigate to vagrant VM"
-  (interactive)
-  (find-file-other-window "/vagrant@192.168.33.10:/home/vagrant/development"))
