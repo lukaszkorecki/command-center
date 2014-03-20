1b276af3c3188d04fbd8ac4dec20179bbf433178
Łukasz Korecki <lukasz@coffeesounds.com>
2014-03-20 21:41:15 +0000

    Add fugitive-like evil commands for working with git

--- settings/evil.el
+++ settings/evil.el
@@ -4,16 +4,36 @@
  "-" 'split-window-vertically
  "ch" 'ruby-convert-hash
  "g" 'projectile-grep
+ "d" 'dired
  "t" 'projectile-find-tag
  "T" 'projectile-regenerate-tags
  "P" 'projectile-find-file-other-window
  "p" 'projectile-find-file)
 
 
-;;; Activate magit like fugitive
-(evil-ex-define-cmd "Git-status" 'magit-status)
+;;; Make magit and emacs behavie like Fugitive
+(defun evil-git-checkout-current-file ()
+  (interactive)
+  (start-process "git-checkout"
+                 (get-buffer-create "*git-checkout*")
+                 "git"
+                 "checkout"
+                 buffer-file-name)
+  (find-file-noselect buffer-file-name))
 
-;;; TODO add more vim mappings to magit!
+(defun evil-git-remove-current-file ()
+  (interactive)
+  (start-process "git-remove"
+                 (get-buffer-create "*git-remove*")
+                 "git"
+                 "rm"
+                 "-f"
+                 buffer-file-name)
+  (kill-buffer))
+
+(evil-ex-define-cmd "Git-status" 'magit-status)
+(evil-ex-define-cmd "Git-remove" 'evil-git-remove-current-file)
+(evil-ex-define-cmd "Git-checkout" 'evil-git-checkout-current-file)
 
 ;;; tags!
 (evil-ex-define-cmd "tag" 'tags-search)
