12c6b1adfc9ddaad9a809259ade99100225d4859
Łukasz Korecki <lukasz@korecki.me>
2016-01-01 11:34:12 +0000

    add special highlighting for compojure

--- settings/lk/clojure.el
+++ settings/lk/clojure.el
@@ -3,6 +3,18 @@
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'linum-mode)
 
+;; add compojure support
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
 (defun lk/fix-with-cljfmt-and-reload ()
   "Run rubocop -a and reloads the buffer."
   (interactive)
