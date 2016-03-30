f21ee811c1cdda2348877d7bfaa6dd41eede94dc
Łukasz Korecki <lukasz@korecki.me>
2016-03-30 15:23:55 +0000

    erb is a web-mode

--- settings/lk/lang.el
+++ settings/lk/lang.el
@@ -39,6 +39,7 @@
 (add-to-list 'auto-mode-alist '("\\.hb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-hook 'web-mode-hook #'linum-mode)
 
 (add-hook 'elisp-mode-hook #'linum-mode)
