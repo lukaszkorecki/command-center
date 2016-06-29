569955eece0a0adfaafd1e610426950fb1d4db44
Łukasz Korecki <lukasz@korecki.me>
2016-06-29 14:25:07 +0000

    Tweaks for the theme

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
 (require 'lk/helm)
 (require 'lk/customizations)
 
+;; make it pretty!
+(setq linum-format " \u2502 ")
+(setq linum-format "%4d \u2502 ")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
 
