18fcc063ba4daf7fad7a0f49a1ea4612e9ba8e50
Łukasz Korecki <lukasz@korecki.me>
2016-09-14 13:17:11 +0000

    Cleanup

--- settings/lk/eyebrowse.el
+++ settings/lk/eyebrowse.el
@@ -3,8 +3,4 @@
 (eyebrowse-mode t)
 (setq custom-file "~/.emacs.d/custom.el")
 
-
-(global-set-key (kbd "C-x >") 'eyebrowse-next-window-config)
-(global-set-key (kbd "C-x <") 'eyebrowse-prev-window-config)
-
 (provide 'lk/eyebrowse)
--- settings/lk/theme.el
+++ settings/lk/theme.el
@@ -4,6 +4,4 @@
 (setq linum-format "%4d \u2502 ")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
-
-
 (provide 'lk/theme)
