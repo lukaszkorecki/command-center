a598325b5013b488e04e6bf619f3a0679f92437b
Łukasz Korecki <lukasz@korecki.me>
2018-04-06 08:57:18 +0200

    Clean up colors in terminal

--- settings/lk/theme.el
+++ settings/lk/theme.el
@@ -1,6 +1,13 @@
-(load-theme 'solarized-light 't)
 ;; set font size
 (when (display-graphic-p)
   (set-face-attribute 'default nil :height 120))
+
+(defun on-after-init ()
+  (unless (display-graphic-p (selected-frame))
+    (set-face-background 'default "unspecified-bg" (selected-frame))))
+
+(add-hook 'window-setup-hook 'on-after-init)
+
+(load-theme 'solarized-dark 't)
 
 (provide 'lk/theme)
