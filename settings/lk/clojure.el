6033b433a77430f3f704d46edda25351c42f4eeb
Łukasz Korecki <lukasz@korecki.me>
2018-05-02 09:22:48 +0100

    More boilerplate

--- settings/lk/clojure.el
+++ settings/lk/clojure.el
@@ -21,14 +21,19 @@
 ;; Adopted from cider's scratch buffer
 
 (defconst lk/clj-scratch-name "*clj-scratch*")
-(defconst lk/clj-scratch-boot
-"(require '[clojure.test :as test]
-          '[clojure.tools.namespace.repl :as ns.repl])")
+(defconst lk/clj-scratch-start-text
+  "(ns scratch
+  (:require [clojure.test :as test]
+            [clojure.tools.namespace.find :as ns.find]
+            [clojure.tools.namespace.repl :as ns.repl]))
+
+(defn find-project-ns []
+  (ns.find/find-namespaces-in-dir (java.io.File. \"src/\")))")
 
 (defun lk/create-clojure-scratch ()
   (with-current-buffer (get-buffer-create lk/clj-scratch-name)
     (clojure-mode)
-    (insert lk/clj-scratch-boot)
+    (insert lk/clj-scratch-start-text)
     (current-buffer)))
 
 (defun lk/clojure-scratch ()
@@ -38,5 +43,10 @@
 
 (global-set-key (kbd "C-x c s") 'lk/clojure-scratch)
 
+(defun lk/open-clojure-repl ()
+  (interactive)
+  (pop-to-buffer (get-buffer "*inf-clojure*")))
+
+(global-set-key (kbd "C-x c r") 'lk/open-clojure-repl)
 
 (provide 'lk/clojure)
