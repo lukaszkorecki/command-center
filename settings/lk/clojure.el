f9a0349a3fe37f79b1843d69b59bf8e5e388d145
Łukasz Korecki <lukasz@korecki.me>
2018-05-23 11:44:20 +0100

    Add lk/clojure-format-current-buffer via cljfmt and better scratch template

--- settings/lk/clojure.el
+++ settings/lk/clojure.el
@@ -1,11 +1,11 @@
 (require 'clojure-mode-extra-font-locking)
 
+(require 'inf-clojure)
 (defun lk/clj-mode-hook ()
-  (rainbow-delimiters-mode t))
+  (rainbow-delimiters-mode t)
+  (inf-clojure-minor-mode t))
 
 (add-hook 'clojure-mode-hook #'lk/clj-mode-hook)
-(require 'inf-clojure)
-(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
 
 ;; add compojure support
 (define-clojure-indent
@@ -18,32 +18,48 @@
   (ANY 2)
   (context 2))
 
-
-;; Adopted from cider's scratch buffer
-
+;; inf-clojure/monroe based clj-scratch buffer
+;; Adopted from cider's scratch
 (defconst lk/clj-scratch-name "*clj-scratch*")
 (defconst lk/clj-scratch-start-text
   "(ns scratch
   (:require [clojure.test :as test]
+            [clojure.repl :as repl]
             [clojure.tools.namespace.find :as ns.find]
-            [clojure.tools.namespace.repl :as ns.repl]
-            [eftest.runner :as ef]))
-;; (ef/run-tests (ef/find-tests \"some.thing\"))
+            [clojure.tools.namespace.repl :as ns.repl]))
+(defn t [p]
+  (test/run-tests (re-pattern p)))
+
+;; Have fun!
 
 ")
 
 (defun lk/create-clojure-scratch ()
+  "Creates a scratch buffer, similar to Emacs' *scratch*
+     and injects template from lk/clj-scratch-start-text"
   (with-current-buffer (get-buffer-create lk/clj-scratch-name)
     (clojure-mode)
     (insert lk/clj-scratch-start-text)
     (current-buffer)))
 
 (defun lk/clojure-scratch ()
+  "Command to create clojure scratch buffer or to switch to it
+     if we have one already"
   (interactive)
   (pop-to-buffer (or (get-buffer lk/clj-scratch-name)
                      (lk/create-clojure-scratch))))
 
 (global-set-key (kbd "C-x c s") 'lk/clojure-scratch)
 
+(defun lk/clojure-format-current-buffer ()
+  "Format current buffer with CljFmt - assume it's installed already
+     (it is as it was added to ~/.lein/profiles.clj)"
+  (interactive)
+  (let ((file-name (buffer-file-name (current-buffer))))
+    (compilation-start
+     (format "lein cljfmt fix %s" file-name)
+     'compilation-mode)))
+
+(global-set-key (kbd "C-x c f") 'lk/clojure-format-current-buffer)
 
 (provide 'lk/clojure)
