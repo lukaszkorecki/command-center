8acc5d88979f4a9c4c305ee18ebb3698abc7d641
Łukasz Korecki <lukasz@korecki.me>
2018-05-25 10:07:31 +0100

    Better socket repl setup and improved clj-scratch template

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,5 @@
-{;; inf-clojure support
- :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]
- ;; for all that repl goodness
- :user {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]]
+{:user {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]]
+        :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]
         :plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
                   [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.15", :exclusions [org.clojure/clojure]]]}}
--- settings/lk/clojure.el
+++ settings/lk/clojure.el
@@ -22,15 +22,22 @@
 ;; Adopted from cider's scratch
 (defconst lk/clj-scratch-name "*clj-scratch*")
 (defconst lk/clj-scratch-start-text
-  "(ns scratch
-  (:require [clojure.test :as test]
-            [clojure.repl :as repl]
-            [clojure.tools.namespace.find :as ns.find]
-            [clojure.tools.namespace.repl :as ns.repl]))
-(defn t [p]
-  (test/run-tests (re-pattern p)))
+  ";; Eval this when starting
+(do
+  (ns scratch
+    (:require [clojure.test :as test]
+              [clojure.repl :as repl]
+              [clojure.tools.namespace.find :as ns.find]
+              [clojure.tools.namespace.repl :as ns.repl]
+              [clojure.java.io :as io])
+    (:import (java.io File)))
+  (defn list-ns-in-pj []
+    (ns.find/find-namespaces-in-dir (File. \"./src/\")))
 
-;; Have fun!
+  (defn t [p]
+    (test/run-all-tests (re-pattern p)))
+  ;; Have fun!
+  )
 
 ")
 
