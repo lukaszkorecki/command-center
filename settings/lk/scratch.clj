b3df74a0a5dbbd3dc538709775b3b76678797d4b
Łukasz Korecki <lukasz@korecki.me>
2018-05-30 07:41:27 +0100

    Improve test reloading and REPL shortcut fns

--- settings/lk/scratch.clj
+++ settings/lk/scratch.clj
@@ -7,10 +7,25 @@
               [clojure.tools.namespace.repl :as ns.repl]
               [clojure.java.io :as io])
     (:import (java.io File)))
+
   (defn list-ns-in-pj []
     (ns.find/find-namespaces-in-dir (File. "./src/")))
 
-  (defn t [p]
-    (test/run-all-tests (re-pattern p))))
+  (defn list-test-ns-in-pj []
+    (ns.find/find-namespaces-in-dir (File. "./test/")))
+
+  (defn require-all []
+    (apply require (conj (vec (list-ns-in-pj)) :reload))
+    (apply require (conj (vec (list-test-ns-in-pj)) :reload)))
+
+  (defn t
+    ([]
+     (require-all)
+     (apply test/run-tests (list-test-ns-in-pj)))
+    ([p]
+     (let [test-nss (->> (list-test-ns-in-pj)
+                         (filter #(re-find (re-pattern p) (str %))))]
+       (apply require (conj (vec test-nss) :reload))
+     (test/run-all-tests (re-pattern p))))))
 
 ;; Have fun!
