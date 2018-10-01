14d1861a58af674722601275c83c20bf79ba7e9b
Łukasz Korecki <lukasz@korecki.me>
2018-10-01 09:55:53 +0100

    Fix projectile mode map and remove obsolete keyboard shortcut

--- settings/lk/ivy.el
+++ settings/lk/ivy.el
@@ -5,17 +5,6 @@
   (ivy-mode 1)
   (bind-key "C-c C-r" 'ivy-resume))
 
-(use-package projectile
-  :ensure t
-  :config
-  (projectile-mode +1)
-  (setq projectile-remember-window-configs t)
-  (setq projectile-git-command "git ls-files -z -c --recurse-submodules")
-  (setq projectile-completion-system 'ivy)
-  (projectile-mode +1)
-  :bind
-  ("C-c n p" . projectile-find-file))
-
 (use-package counsel
   :ensure t
   :bind
@@ -23,5 +12,15 @@
   ("C-c n i" . counsel-imenu)
   ("C-c n b" . counsel-ibuffer)
   ("C-c n g" . counsel-git-grep))
+
+(use-package projectile
+  :after ivy
+  :ensure t
+  :init
+  (setq projectile-completion-system 'ivy)
+  :config
+  (setq projectile-remember-window-configs t)
+  (setq projectile-git-command "git ls-files -z -c --recurse-submodules")
+  (projectile-mode +1))
 
 (provide 'lk/ivy)
