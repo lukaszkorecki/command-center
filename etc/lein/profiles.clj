decb3aaa75b256bc0740cdb2393386ecbee2316d
Łukasz Korecki <lukasz@korecki.me>
2018-10-30 08:46:11 +0100

    Reorganize global Lein profie

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]
-                       [com.cemerick/pomegranate "1.0.0"]
+{:repl {:dependencies [^:displace [org.clojure/clojure "1.9.0"]
+                       [org.clojure/tools.namespace "0.3.0-alpha4"]
                        [eftest "0.5.2"]]
-        :repl-options {:color false}
-        :plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
+        :repl-options {:color false}}
+ :user {:plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
                   [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.3" :exclusions [org.clojure/clojure]]
                   [lein-eftest "0.5.2" :exclusions [org.clojure/clojure]]
