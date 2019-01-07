9952498bcfbe761d529ebe703fd0f106e2a0bc96
Łukasz Korecki <lukasz@korecki.me>
2019-01-07 17:28:42 +0000

    Add a binding for ruby-mode+rubocop

--- settings/lk/ruby.el
+++ settings/lk/ruby.el
@@ -1,6 +1,16 @@
 ;;; lk/ruby --- ruby related customizations
 ;;;; Commentary:
 ;;; Code:
+
+(defun lk/ruby-format-current-buffer ()
+  "Format current buffer with CljFmt - assume it's installed already
+     (it is as it was added to ~/.lein/profiles.clj)"
+  (interactive)
+  (let ((file-name (buffer-file-name (current-buffer))))
+    (compilation-start
+     (format "rubocop -a %s" file-name)
+     'compilation-mode)))
+
 (use-package ruby-mode
   :ensure t
   :init
@@ -10,7 +20,9 @@
   (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
-  (setq-default ruby-indent-level 2))
+  (setq-default ruby-indent-level 2)
+  :bind
+  (("C-x c f" .  lk/ruby-format-current-buffer)))
 
 (provide 'lk/ruby)
 ;;; ruby.el ends here
