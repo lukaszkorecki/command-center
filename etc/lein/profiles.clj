be6cbbbd816eb4f0500b4bca44dc8e04a969b20a
Łukasz Korecki <lukasz@korecki.me>
2019-05-07 16:35:03 -0400

    Restructure lein profile

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,8 +1,8 @@
 {:repl {:dependencies [^:displace [org.clojure/clojure "1.9.0"]
                        [org.clojure/tools.namespace "0.3.0-alpha4"]
+                       [cljfmt "0.6.4"]
                        [lambdaisland/kaocha "0.0-389"]]
         :repl-options {:color false}}
- :user {:plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
-                  [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
-                  [lein-kibit "0.1.3" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.15", :exclusions [org.clojure/clojure]]]}}
+ :user {:plugins [[lein-cljfmt "0.6.4"]
+                  [lein-cloverage "1.1.1"]
+                  [lein-ancient "0.6.15"]]}}
