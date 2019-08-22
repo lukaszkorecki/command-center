f6e79d6eb19b9685fdd069d0b1326d761fb923dc
Łukasz Korecki <lukasz@korecki.me>
2019-08-22 11:24:08 -0700

    Add Vagrant info

--- vm/provision.sh
+++ vm/provision.sh
@@ -1,31 +1,56 @@
+#!/usr/bin/env bash
+
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Etc/UTC" > /etc/timezone
-sudo dpkg-reconfigure -f noninteractive tzdata
+if [[ $(cat /etc/timezone) != "Etc/UTC" ]] ; then
+  echo "Etc/UTC" > /etc/timezone
+  sudo dpkg-reconfigure -f noninteractive tzdata
+
+fi
 
 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+if [[ "$(which curl)" == "" ]] ; then
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+fi
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+if [[ "$(which gcloud)" == "" ]] ; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-sudo apt -y update
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
-sudo apt install -y yarn docker-ce \
-     emacs25 emacs25-common \
-     tmux curl ruby2.5 ruby2.5-dev \
-     zlib1g-dev liblzma-dev \
-     build-essential patch \
-     openjdk-8-jre-headless \
-     libpq-dev \
-     google-cloud-sdk
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-sudo gem install bundler -v 1.17.1
+  sudo apt -y update
+
+  sudo apt install -y yarn docker-ce \
+       emacs25 emacs25-common \
+       tmux curl ruby2.5 ruby2.5-dev \
+       zlib1g-dev liblzma-dev \
+       build-essential patch \
+       openjdk-8-jre-headless \
+       libpq-dev \
+       google-cloud-sdk
+
+  sudo usermod -aG docker vagrant
+fi
+
+if [[ "$(which bundler)" == "" ]] ; then
+  sudo gem install bundler -v 1.17.1
+fi
 
 
-curl -s https://raw.githubusercontent.com/envkey/envkey-source/master/install.sh | bash
+
+
+if [[ "$(which envkey-source)" == "" ]] ; then
+  curl -s https://raw.githubusercontent.com/envkey/envkey-source/master/install.sh | bash
+fi
+
+
+if [[ "$(which docker-compose)" == "" ]] ; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
