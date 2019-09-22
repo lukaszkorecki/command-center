d451e7452c63055131fd18f2fe95d0c7e12e24dd
Łukasz Korecki <lukasz@korecki.me>
2019-09-22 20:19:30 +0000

    DRY

--- etc/repl.clj
+++ etc/repl.clj
@@ -14,29 +14,17 @@
 
 (defn help []
   (println (str ">>> in ns " *ns*))
-  (println "(R/t) - run all tests
-(R/find-test-ns  #\"some-regex\") - find test namespace matching regex
-(R/find-ns  #\"some-regex\") - find namespace matching regex
-(R/t (R/find-test-ns  #\"some-regex\")) - run tests for matching namespaces
-(R/t! (R/find-test-ns  #\"some-regex\")) - run tests for matching namespaces but also refresh (dangerous!)
-(R/time+ \"tag\"  (some expr)) - like time, but better
-(R/refresh) - refresh all namespaces
-(R/refresh-all) - refresh all project + dep namespaces
-(R/pp) alias for clojure.pprint/pprint
-(R/list-ns) - find all namespaces in SRC
-(R/start-system! 'some.user-ns) - refresh and start a system in some.user-ns
-(R/stop-system! 'some.user-ns) - stop a system in some.user-ns
-(R/c :component-name) - pull out compoentn from a running system
-(R/add-dependency ['cheshire \"5.8.1\"]) - add a library to current repl process"))
+  (mapv (fn [[k v]]
+          (println v)) (ns-publics *ns*)))
 
 (defn init! []
   (ns scratch)
   (ns.repl/disable-reload! *ns*)
   (help))
 
-(def refresh ns.repl/refresh)
-(def refresh-all ns.repl/refresh-all)
-(def pp clojure.pprint/pprint)
+(def ^{:doc "Refresh changed namespaces"}  refresh ns.repl/refresh)
+(def ^{:doc "Refresh everything"}  refresh-all ns.repl/refresh-all)
+(def ^{:doc "Pretty pretint a thing"} pp clojure.pprint/pprint)
 
 (defn list-ns
   "Return list of symbols of namespaces found in src dir"
@@ -45,19 +33,25 @@
   ([]
    (list-ns "./src/")))
 
-(defn find-ns [re]
+(defn find-ns
+  "Find namespace vars by a regex"
+  [re]
   (filter #(re-find re (str %)) (list-ns)))
 
-(defn find-test-ns [re]
+(defn find-test-ns
+  "Find test namespace vars by a regex"
+  [re]
   (filter #(re-find re (str %)) (list-ns "./test/")))
 
 (defn t
+  "Run tests via kaocha - either all or a list of vars"
   ([]
    (kaocha.repl/run :unit))
   ([ns-list]
    (apply kaocha.repl/run ns-list)))
 
 (defn t!
+  "Run tests via kaocha, but refresh first - runs all tests or a list of vars"
   ([]
    (refresh)
    (kaocha.repl/run :unit))
@@ -72,7 +66,9 @@
   - refresh
   - require the user ns
   - start  system, invoking somer-service.user/start
-  Warning: best if the system is not running, or things will go south"
+  Warning: best if the system is not running, or things will go south
+
+  Example: (R/start-system! 'foo.user)"
   [an-ns]
   (if (get @system-status an-ns)
     (println "!!  System possibly running")
@@ -85,7 +81,7 @@
         (swap! system-status (fn [s] (assoc s an-ns true)))))))
 
 (defn stop-system!
-  "Given a namespace, usually some-service.user, stop the system"
+  "Given a namespace, usually some-service.user, stop the system. If not passed, stops currently running system"
   ([]
    (stop-system! (first (keys @system-status))))
   ([an-ns]
@@ -94,7 +90,7 @@
     (swap! system-status (fn [s] (assoc s an-ns false))))))
 
 (defn c
-  "Pul out a compont from a system"
+  "Pul out a compont from a running system"
   [component-name]
   (let [sys (var-get (ns-resolve (first (keys @system-status)) 'SYS))]
     (get sys component-name)))
