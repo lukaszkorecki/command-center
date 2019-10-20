0dc5e207328fff66470513adc5294de0e4cd7225
Łukasz Korecki <lukasz@korecki.me>
2019-10-20 20:18:38 +0000

    Update lein profile

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,13 +1,10 @@
-{:repl {:dependencies [^:displace [org.clojure/clojure "1.10.0"]
-                       [com.cemerick/pomegranate "0.4.0"]
-                       [lambdaisland/kaocha "0.0-389"]
-                       [org.clojure/tools.namespace "0.3.0"]]
+{:repl {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
+                       [com.cemerick/pomegranate "1.1.0"]
+                       [lambdaisland/kaocha "0.0-554"]
+                       [org.clojure/tools.namespace "0.3.1"]]
         :repl-options {:color false}}
- :user {:dependencies [^:displace [org.clojure/clojure "1.10.0"]
-
-                       [cljfmt "0.6.4"]
-                       [slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+ :user {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
+                       [cljfmt "0.6.4"]]
         :plugins [[lein-cljfmt "0.6.4"]
                   [lein-cloverage "1.1.1"]
                   [lein-ancient "0.6.15"]]}}
