983fb65e046b7b7a435eeb9ab8b64c49132ecd7f
Łukasz Korecki <lukasz@korecki.me>
2019-10-21 21:09:44 +0000

    Fix utf-8 handling in the terminal

--- init.el
+++ init.el
@@ -61,3 +61,13 @@
 ;; load modeline
 (require 'lk/modeline)
 (put 'narrow-to-region 'disabled nil)
+
+
+;; unicode reules everything around me
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(define-coding-system-alias 'UTF-8 'utf-8)
--- settings/lk/editing.el
+++ settings/lk/editing.el
@@ -1,9 +1,3 @@
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-
 ;; strip whitespace
 (use-package whitespace
   :ensure t
