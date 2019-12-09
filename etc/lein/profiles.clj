146dddd057a0c45955932e409393f3f1731c40b4
Łukasz Korecki <lukasz@korecki.me>
2019-12-09 16:59:31 +0000

    Bump lein deps

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,10 +1,10 @@
 {:repl {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
                        [com.cemerick/pomegranate "1.1.0"]
                        [lambdaisland/kaocha "0.0-554"]
-                       [org.clojure/tools.namespace "0.3.1"]]
+                       [nrepl "0.6.0"]
+                       [org.clojure/tools.namespace "0.3.2-SNAPSHOT"]]
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
-                       [cljfmt "0.6.4"]]
-        :plugins [[lein-cljfmt "0.6.4"]
-                  [lein-cloverage "1.1.1"]
+                       [cljfmt "0.6.6"]]
+        :plugins [[lein-cljfmt "0.6.6"]
                   [lein-ancient "0.6.15"]]}}
