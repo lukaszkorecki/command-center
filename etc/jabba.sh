15a7a9816bfb5b204a9d8b63357db0690b307a40
Łukasz Korecki <lukasz@korecki.me>
2020-01-05 10:09:40 -0800

    Parametrize JVM version

--- etc/jabba.sh
+++ etc/jabba.sh
@@ -1,13 +1,14 @@
+JAVA_VERSION=adopt@1.11.0-5
 
 jabba-install() {
   curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
-  jabba install adopt@1.11.28-0
-  jabba use adopt@1.11.28-0
+  jabba install $JAVA_VERSION
+  jabba use $JAVA_VERSION
 }
 
 if [[ -s "/home/vagrant/.jabba/jabba.sh" ]] ; then
   source "/home/vagrant/.jabba/jabba.sh"
-  jabba use adopt@1.11.28-0
+  jabba use $JAVA_VERSION
 else
   echo Jabba not installed!
   echo "run jabba-install"
