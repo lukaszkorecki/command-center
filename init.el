5170176961085745e0cb334a50c67162952c2eb0
Łukasz Korecki <lukasz@korecki.me>
2020-01-27 08:23:25 -0800

    Speed up startup time

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;;; init --- loads all customizations and packages
 ;;;; Commentary - Prolly needs splitting more
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load-file "~/.emacs.d/deps.el")
 
 (eval-when-compile
