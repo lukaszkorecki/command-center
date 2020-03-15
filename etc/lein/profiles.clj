28a36d34cea789a69a507f011badc0163ce1fb73
Łukasz Korecki <lukasz@korecki.me>
2020-03-15 13:38:45 -0700

    Update kaocha and add a shell runner

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,9 +1,12 @@
 {:repl {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
-                       [lambdaisland/kaocha "0.0-554"]
+                       [lambdaisland/kaocha "0.0-601"]
                        [nrepl "0.6.0"]
                        [org.clojure/tools.namespace "0.3.1"]]
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
                        [cljfmt "0.6.6"]]
         :plugins [[lein-cljfmt "0.6.6"]
-                  [lein-ancient "0.6.15"]]}}
+                  [lein-ancient "0.6.15"]]}
+
+ :kaocha {:dependencies [[lambdaisland/kaocha "0.0-601"]]}
+ }
