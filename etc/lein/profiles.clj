7f1307ee6c6ca9ba6d31b0840377d2ee3feaa6ed
Łukasz Korecki <lukasz@korecki.me>
2020-05-15 14:54:25 -0700

    Remove cljfmt

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -4,9 +4,8 @@
                        [org.clojure/tools.namespace "0.3.1"]]
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
-                       [cljfmt "0.6.6"]]
-        :plugins [[lein-cljfmt "0.6.6"]
-                  [lein-ancient "0.6.15"]]}
+                       ]
+        :plugins [[lein-ancient "0.6.15"]]}
 
  :kaocha {:dependencies [[lambdaisland/kaocha "0.0-601"]]}
  }
