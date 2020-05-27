56ee49cb6db5cb4a70b3e0bb8230eaabd2690209
Łukasz Korecki <lukasz@korecki.me>
2020-05-27 00:10:27 -0700

    Active which-key on demand, not automatically

--- settings/lk/customizations.el
+++ settings/lk/customizations.el
@@ -34,7 +34,14 @@
 
 (use-package which-key
   :ensure t
-  :init (which-key-mode))
+  :init
+  (setq which-key-show-early-on-C-h t)
+  ;; make sure which-key doesn't show normally but refreshes quickly after it is
+  ;; triggered.
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05)
+  (which-key-mode))
+
 
 (provide 'lk/customizations)
 ;;; customizations.el ends here
