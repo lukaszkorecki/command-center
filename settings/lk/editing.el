2c65cfe226208665b8ef1c5545979bd4544ef616
Łukasz Korecki <lukasz@korecki.me>
2020-10-12 08:56:41 -0700

    [emacs] Disable C-x z (dont minimize)

--- settings/lk/editing.el
+++ settings/lk/editing.el
@@ -54,6 +54,7 @@ Based on  `http://ergoemacs.org/emacs/emacs_show_kill_ring.html'"
 
 ;; disable c-z which maps to minimize
 (global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x z"))
 ;; set in projectile later, originally used in dired/tramp
 (global-unset-key (kbd "C-c n p"))
 
