60b2a12451131dab922f49e4d74d3618ad3a296a
Łukasz Korecki <lukasz@korecki.me>
2021-02-17 08:09:15 -0800

    Fix and speed up alfred helper script

--- alfred-scripts/project-browse/project_dir.rb
+++ alfred-scripts/project-browse/project_dir.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'fileutils'
 require 'json'
 
-CACHE_LOCATION = "~/.cache/project_browse_cache.json"
+CACHE_LOCATION = '~/.cache/project_browse_cache.json'
 PROJECTS_DIR_ROOT = '~/pj/'
 
 cache_path = File.expand_path(CACHE_LOCATION)
-FileUtils.touch(cache_path) if ! File.exist?(cache_path)
+FileUtils.touch(cache_path) unless File.exist?(cache_path)
 
 cache = File.new(cache_path, 'r+')
 cached_data = cache.read
 
-cache_empty = cached_data.size == 0
+cache_empty = cached_data.size.zero?
 
 now = Time.now.to_i
 last_time = cache.mtime.to_i
@@ -20,32 +22,30 @@ diff_in_minutes = (now - last_time) / 60
 
 $stderr.write "Diff: ##{diff_in_minutes}"
 
-if cache_empty ||  diff_in_minutes > 4 * 60 # if 4 hours old, rebuild the cache
+if cache_empty || diff_in_minutes > 4 * 60 # if 4 hours old, rebuild the cache
   $stderr.write 'cache is old, recreating'
   root = File.expand_path(PROJECTS_DIR_ROOT)
 
-  list =  Dir[root + '/**/.git'].map { |x| File.dirname x }
-
+  list =  Dir["#{root}/**/.git"].map { |x| File.dirname x }
 
   items = list.map do |path|
     name = File.basename(path)
     {
       uid: name,
       title: name,
-      subtitle: "",
+      subtitle: '',
       arg: path,
       autocomplete: name,
-      icon: { type: "fileicon", path: path }
+      icon: { type: 'fileicon', path: path }
     }
   end
 
   json = { items: items }.to_json
 
-  cache.write  json
-  puts(json)
+  cache.write json
 
-else
-  puts cached_data
+  cached_data = json
 end
-
 cache.close
+
+puts JSON.parse(cached_data).to_json
