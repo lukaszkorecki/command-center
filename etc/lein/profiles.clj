cc0d3f83e12423a3bfe9f7d029166d3caf6de8be
Łukasz Korecki <lukasz@korecki.me>
2021-11-20 08:56:39 -0800

    [clj] Update lein profile stuff

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,10 +1,9 @@
 {:repl {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
-                       [lambdaisland/kaocha "1.0.672"]
-                       [nrepl "0.8.0"]
-                       [org.clojure/tools.namespace "0.3.1"]]
+                       [lambdaisland/kaocha "1.60.945"]
+                       [nrepl "0.8.3"]
+                       [org.clojure/tools.namespace "1.1.0"]]
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]]
-        :plugins [[lein-ancient "0.6.15"]
+        :plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-nvd "1.7.0"]
-                  [lein-licenses "0.2.2"]]}
- :kaocha {:dependencies [[lambdaisland/kaocha "1.0.672"]]}}
+                  [lein-licenses "0.2.2"]]}}
