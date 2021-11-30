82e17174feea3d33eaa17ed5c837d70b58a2eff4
Łukasz Korecki <lukasz@korecki.me>
2021-11-30 12:11:59 -0800

    Add a lein alias

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -4,6 +4,7 @@
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
                        [lambdaisland/kaocha "1.60.945"]]
+        :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
         :plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-nvd "1.7.0"]
                   [lein-licenses "0.2.2"]]}}
