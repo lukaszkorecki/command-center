ce1007d9c5ba6f6c3c034e755354a5710c69b207
Łukasz Korecki <lukasz@korecki.me>
2021-11-30 12:09:01 -0800

    [clj] Tweak profile so that kaocha.runner works out of the box

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,9 +1,9 @@
-{:repl {:dependencies [^:displace [org.clojure/clojure "1.10.1"]
-                       [lambdaisland/kaocha "1.60.945"]
+{:repl {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
                        [nrepl "0.8.3"]
                        [org.clojure/tools.namespace "1.1.0"]]
         :repl-options {:color false}}
- :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]]
+ :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
+                       [lambdaisland/kaocha "1.60.945"]]
         :plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-nvd "1.7.0"]
                   [lein-licenses "0.2.2"]]}}
