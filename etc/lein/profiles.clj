e7dbbfd51a24f7bcbe66232acb5c09cc70bd930a
Łukasz Korecki <lukasz@korecki.me>
2022-02-17 07:55:41 -0800

    Load ssh keys if not already loaded

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -4,7 +4,7 @@
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
                        [org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT"]]
         :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
-        ;; :injections [ (require '[rumble.repl :as R])]
+        :injections [ (require '[rumble.repl :as R])]
         :plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-nvd "1.7.0"]
                   [lein-licenses "0.2.2"]]}}
