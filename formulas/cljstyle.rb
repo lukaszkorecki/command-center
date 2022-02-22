fb1565d603ce1090a2d9d3f5b73eb2de5a08698e
Łukasz Korecki <lukasz@korecki.me>
2022-02-22 09:14:25 -0800

    fix up brew stuff

--- formulas/cljstyle.rb
+++ formulas/cljstyle.rb
@@ -3,7 +3,7 @@ class Cljstyle < Formula
 
   version VERSION
   url "https://github.com/greglook/cljstyle/releases/download/#{VERSION}/cljstyle_#{VERSION}_macos.tar.gz"
-
+  sha256 "7435e8fe23b5d25028b88906092b3cd6c93e976ca75f39a55fe7f8927696fbe8"
 
   def install
     bin.install "cljstyle"
--- formulas/clojure-lsp.rb
+++ formulas/clojure-lsp.rb
@@ -3,6 +3,7 @@ class ClojureLsp < Formula
 
   version LSP_VERSION
   url "https://github.com/clojure-lsp/clojure-lsp/releases/download/#{LSP_VERSION}/clojure-lsp-native-macos-amd64.zip"
+  sha256 "6c53000ca9f4cee8164f9b737a28e506dde26b3991fa5d5145cb8124dc71ba68"
 
 
   def install
