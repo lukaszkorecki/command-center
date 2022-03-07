3ad99e602c6d4b27bb83284632de2b39966c8b9a
Łukasz Korecki <lukasz@korecki.me>
2022-03-07 08:09:18 -0800

    fix lein profile

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,10 +1,12 @@
-{:repl {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
-                       [nrepl "0.8.3"]]
+{:repl {:dependencies [[org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT-2"]
+                       ^:displace [nrepl "0.9.0"]]
+        :injections [ (require '[rumble.repl :as R])]
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
-                       [org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT-2"]]
+                       [lambdaisland/kaocha "1.63.998"] ;; for the alias
+                       ]
         :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
-        :injections [ (require '[rumble.repl :as R])]
+
         :plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-nvd "1.7.0"]
                   [lein-licenses "0.2.2"]]}}
