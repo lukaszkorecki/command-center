d1a2d952b8938a745b62aa7c8f0e826740d8cc15
Łukasz Korecki <lukasz@korecki.me>
2022-04-17 10:20:21 -0700

    Use latest rumble

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,10 +1,10 @@
-{:repl {:dependencies [[org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT-2"]
+{:repl {:dependencies [[org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT-3"]
                        ^:displace [nrepl "0.9.0"]]
-        :injections [(require '[rumble.repl :as R])]
+        :injections [(require '[rumble.repl :as rr])]
         :repl-options {:color false}}
- :dev {:injections [(require '[rumble.repl :as R])]}
+ :dev {:injections [(require '[rumble.repl :as rr])]}
  :user {:dependencies [^:displace [org.clojure/clojure "1.10.3"]
-                       [lambdaisland/kaocha "1.63.998"] ;; for the alias
+                       [lambdaisland/kaocha "1.65.1029"] ;; for the alias
                        ]
         :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
         :plugins [[lein-ancient "1.0.0-RC3"]
