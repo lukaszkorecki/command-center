5ed54528c7099e4bb65734f84295890027cbefcf
Łukasz Korecki <lukasz@korecki.me>
2022-06-12 11:44:27 -0700

    make openjdk work on M1 mac when installed via new homebrew

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -2,7 +2,7 @@
                        ^:displace [nrepl "0.9.0"]]
         :repl-options {:color false}}
  :user {:dependencies [^:displace [org.clojure/clojure "1.11.1"]
-                       [lambdaisland/kaocha "1.66.1034"] ;; for the alias
+                       [lambdaisland/kaocha "1.66.1034"] ; for the alias
                        ]
         :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
         :plugins [[lein-ancient "1.0.0-RC3"]
