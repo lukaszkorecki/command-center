36836f600065f3df32a8779fe9221efe58a73acb
Łukasz Korecki <lukasz@korecki.me>
2022-10-22 08:57:37 -0700

    Try eglot

--- settings/lk/lsp.el
+++ settings/lk/lsp.el
@@ -3,39 +3,55 @@
 ;;; Only Clojure is supported, other languages might be added
 
 ;;; Code:
-(use-package lsp-mode
-  :init (setq lsp-keymap-prefix "C-c l")
-  ;; (setq lsp-clojure-custom-server-command "/usr/local/bin/clojure-lsp")
-  (setq lsp-headerline-breadcrumb-mode nil)
-  (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-keep-workspace-alive nil)
-  (setq lsp-file-watch-threshold 1000)
-  (setq lsp-restart nil)
-  (setq lsp-ui-sideline-enable nil)
-  :config (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\node_modules\\'")
-  :hook ((clojure-mode . lsp)
-         (typescript-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp
-  :bind (("C-c l a" . lsp-ui-sideline--code-actions)))
+;; (use-package lsp-mode
+;;   :init (setq lsp-keymap-prefix "C-c l")
+;;   ;; (setq lsp-clojure-custom-server-command "/usr/local/bin/clojure-lsp")
+;;   (setq lsp-headerline-breadcrumb-mode nil)
+;;   (setq lsp-headerline-breadcrumb-enable nil)
+;;   (setq lsp-headerline-breadcrumb-enable nil)
+;;   (setq lsp-keep-workspace-alive nil)
+;;   (setq lsp-file-watch-threshold 1000)
+;;   (setq lsp-restart nil)
+;;   (setq lsp-ui-sideline-enable nil)
+;;   :config (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\node_modules\\'")
+;;   :hook ((clojure-mode . lsp)
+;;          (typescript-mode . lsp)
+;;          (lsp-mode . lsp-enable-which-key-integration))
+;;   :commands lsp
+;;   :bind (("C-c l a" . lsp-ui-sideline--code-actions)))
 
-(use-package lsp-ivy
+;; (use-package lsp-ivy
 
-  :commands lsp-ivy-workspace-symbol
-  :bind (("C-c w i" . lsp-ivy-workspace-symbol)))
+;;   :commands lsp-ivy-workspace-symbol
+;;   :bind (("C-c w i" . lsp-ivy-workspace-symbol)))
 
-(use-package lsp-treemacs
+;; (use-package lsp-treemacs
 
-  :commands lsp-treemacs-errors-list)
+;;   :commands lsp-treemacs-errors-list)
 
 
-(use-package lsp-ui)
+;; (use-package lsp-ui)
 
-(use-package flycheck
+;; (use-package flycheck
 
-  :init (global-flycheck-mode)
-  :bind (("C-c n e" . flycheck-next-error)))
+;;   :init (global-flycheck-mode)
+;;   :bind (("C-c n e" . flycheck-next-error)))
+
+
+(use-package eglot
+  :custom
+  (eglot-confirm-server-initiated-edits nil)
+  (eglot-connect-timeout 300)
+  :config
+  (setq eglot-autoshutdown t)
+  :hook ((clojure-mode . eglot-ensure)
+         (typescript-mode . eglot-ensure))
+  :bind (("C-c l r r" . eglot-rename)
+        ("C-c l f" . eglot-find-declaration)
+        ("C-c l a" . eglot-code-actions )))
+
+(use-package flymake
+  :bind (( "C-c n e" . flymake-goto-next-error )))
 
 (provide 'lk/lsp)
 ;;; lsp.el ends here
