a413063a467022d5e4a79ff7591fd7d699f02083
Łukasz Korecki <lukasz@korecki.me>
2022-11-01 08:57:27 -0700

    Potentially fix project discovery in eglot

--- settings/lk/lsp.el
+++ settings/lk/lsp.el
@@ -59,5 +59,26 @@
          ( "C-c e p" . flymake-goto-prev-error )
          ( "C-c e l" . flymake-show-buffer-diagnostics )))
 
+
+;; FIXME: this overlaps with projectile and it's annoying as fuck.... fix somehow - either migrate of
+;; projectile (since I use it very little?) or just leave this here...?
+;; see nav.el for projectile-specific configuration
+(setq lk/project-files
+      '("project.clj" "deps.edn"  "Gemfile"  "package.json" "Makefile" "main.tf" ".git"))
+
+(defun project-find-root (path)
+  "Search up the PATH for known project file markers"
+  (when-let ((root (first
+                    (seq-filter
+                     (lambda (s) s)
+                     (mapcar
+                      (lambda (f)
+                        (locate-dominating-file default-directory f))
+                      lk/project-files)))))
+    (cons 'transient (expand-file-name root))))
+
+
+(add-to-list 'project-find-functions #'project-find-root)
+
 (provide 'lk/lsp)
 ;;; lsp.el ends here
