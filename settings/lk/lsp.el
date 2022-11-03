c162df379ab076a7c0a850f36c81e5f299fc9d02
Łukasz Korecki <lukasz@korecki.me>
2022-11-03 10:32:59 -0700

    Clean up LSP config

--- settings/lk/lsp.el
+++ settings/lk/lsp.el
@@ -1,42 +1,6 @@
 ;;; lsp.el -- LSP configuration + supporting packages
 ;;; Commentary:
-;;; Only Clojure is supported, other languages might be added
-
-;;; Code:
-;; (use-package lsp-mode
-;;   :init (setq lsp-keymap-prefix "C-c l")
-;;   ;; (setq lsp-clojure-custom-server-command "/usr/local/bin/clojure-lsp")
-;;   (setq lsp-headerline-breadcrumb-mode nil)
-;;   (setq lsp-headerline-breadcrumb-enable nil)
-;;   (setq lsp-headerline-breadcrumb-enable nil)
-;;   (setq lsp-keep-workspace-alive nil)
-;;   (setq lsp-file-watch-threshold 1000)
-;;   (setq lsp-restart nil)
-;;   (setq lsp-ui-sideline-enable nil)
-;;   :config (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\node_modules\\'")
-;;   :hook ((clojure-mode . lsp)
-;;          (typescript-mode . lsp)
-;;          (lsp-mode . lsp-enable-which-key-integration))
-;;   :commands lsp
-;;   :bind (("C-c l a" . lsp-ui-sideline--code-actions)))
-
-;; (use-package lsp-ivy
-
-;;   :commands lsp-ivy-workspace-symbol
-;;   :bind (("C-c w i" . lsp-ivy-workspace-symbol)))
-
-;; (use-package lsp-treemacs
-
-;;   :commands lsp-treemacs-errors-list)
-
-
-;; (use-package lsp-ui)
-
-;; (use-package flycheck
-
-;;   :init (global-flycheck-mode)
-;;   :bind (("C-c n e" . flycheck-next-error)))
-
+;;; Only Clojure is fully supported, other languages might be added
 
 (use-package eglot
   :custom
@@ -51,13 +15,12 @@
   :bind (("C-c l r r" . eglot-rename)
         ("C-c l f" . eglot-find-declaration)
         ("C-c l a" . eglot-code-actions )
-        ("C-c l g" . xref-find-definitions)
-        ))
+        ("C-c l g" . xref-find-definitions)))
 
 (use-package flymake
   :bind (( "C-c e n" . flymake-goto-next-error )
          ( "C-c e p" . flymake-goto-prev-error )
-         ( "C-c e l" . flymake-show-buffer-diagnostics )))
+         ( "C-c e l" . flymake-show-buffer-diagnostics)))
 
 
 ;; FIXME: this overlaps with projectile and it's annoying as fuck.... fix somehow - either migrate of
