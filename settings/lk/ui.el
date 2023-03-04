e8dbaa96a3820101eda1594abe12bcb3a8186bd3
Łukasz Korecki <lukasz@korecki.me>
2023-03-04 15:27:18 -0800

    add marginalia

--- settings/lk/ui.el
+++ settings/lk/ui.el
@@ -69,6 +69,19 @@
   :init (require 'bufler)
   :bind (("C-x C-b" . bufler-list)))
 
+(use-package marginalia
+  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
 (use-package undo-tree
   :init (global-undo-tree-mode nil)
   :config
