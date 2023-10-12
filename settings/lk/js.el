ac3d45a19147bc6e3219f8ea9800107724f7ca9a
Łukasz Korecki <lukasz@korecki.me>
2023-10-12 08:40:33 -0700

    Do not enable prettier everywhere

--- settings/lk/js.el
+++ settings/lk/js.el
@@ -3,41 +3,33 @@
 
 ;;; Code:
 
-;; Javascripts
-(defun lk/prettier-format-current-buffer ()
-  "."
-  (interactive)
-  (lk/invoke-compile-tool-in-project "FORCE_COLOR=0 node ./node_modules/.bin/prettier --write %s"))
-
+;; Javascripts, Typescripts and all that crap
+(use-package prettier :ensure t)
 
 (use-package tsx-ts-mode
   :straight (:host github
                    :repo "tree-sitter/tree-sitter-typescript"
                    :path "tsx/src")
   :ensure t
+  :after (prettier)
   :init (add-to-list 'auto-mode-alist '("\\.tsx$" . tsx-ts-mode))
+  (add-hook 'tsx-ts-mode-hook 'prettier-mode)
   :bind (:map tsx-ts-mode-map
               (( "C-x c f" . lk/prettier-format-current-buffer )))
   (:map typescript-mode-map
         (( "C-x c f" . lk/prettier-format-current-buffer ))))
 
 (use-package typescript-ts-mode
-  :after (tsx-ts-mode)
+  :after (prettier tsx-ts-mode)
   :init (setq typescript-indent-level 2)
+  (add-hook 'typescript-mode-hook 'prettier-mode)
+  (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
   (add-to-list 'majo-mode-remap-alist
                '(typescript-mode . typescript-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
-  ;; :bind
-  ;; (:map typescript-ts-mode-map (( "C-x c f" . eglot-format-buffer )))
-  ;; (:map tsx-ts-mode-map (( "C-x c f" . eglot-format-buffer)))
-  ;; (:map typescript-mode-map (( "C-x c f" . eglot-format-buffer)))
-  )
+  (add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode)))
 
 
-(use-package prettier
-  :ensure t
-  :init
-  (global-prettier-mode t))
+
 
 (provide 'lk/js)
 ;;; js.el ends here
