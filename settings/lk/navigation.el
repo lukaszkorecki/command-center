a9e4eb83b10c1c3d550a7da8e783b8c8f2e1678d
Łukasz Korecki <lukasz@korecki.me>
2023-11-04 09:46:12 -0700

    Add fn to purge dired buffers

--- settings/lk/navigation.el
+++ settings/lk/navigation.el
@@ -5,11 +5,9 @@
 
 (use-package ivy
   :diminish ivy-mode
-  :config
-  (setq ivy-height 25)
+  :config (setq ivy-height 25)
   (ivy-mode 1)
-  :custom
-    (ivy-use-virtual-buffers t)
+  :custom (ivy-use-virtual-buffers t)
   (counsel-switch-buffer-preview-virtual-buffers nil)
   :bind (("C-c s" . swiper)
          ("C-c C-r" . ivy-resume)))
@@ -21,16 +19,14 @@
          ("C-c n y" . counsel-yank-pop)))
 
 (use-package projectile
-  :init
-  (projectile-mode +1)
-  :bind
-  (:map projectile-mode-map
-        ("C-c p" . projectile-command-map)
-        (("C-c C-g" . 'projectile-grep)))
-  :config
-  (setq projectile-completion-system 'ivy)
+  :init (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)
+              (("C-c C-g" . 'projectile-grep)))
+  :config (setq projectile-completion-system 'ivy)
   (setq projectile-git-command "git ls-files -z -c --recurse-submodules")
   (add-to-list 'projectile-globally-ignored-directories "vendor")
+  (add-to-list 'projectile-globally-ignored-directories ".git")
   (add-to-list 'projectile-globally-ignored-directories "node_modules")
   (add-to-list 'projectile-globally-ignored-directories "target")
   (setq projectile-project-root-functions
@@ -39,29 +35,24 @@
           projectile-root-bottom-up
           projectile-root-top-down-recurring)))
 
-(use-package counsel-projectile
-  :init (counsel-projectile-mode))
+(use-package counsel-projectile :init (counsel-projectile-mode))
 
 (use-package all-the-icons-ivy-rich
   :ensure t
   :init (all-the-icons-ivy-rich-mode 1))
 
-(use-package ivy-rich
-  :ensure t
-  :init (ivy-rich-mode 1))
+(use-package ivy-rich :ensure t :init (ivy-rich-mode 1))
 
 (use-package ace-window
-  :config
-  (setq aw-keys '(?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
+  :config (setq aw-keys '(?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
   (setq aw-ignore-current nil)
   (setq aw-dispatch-always t)
   (setq aw-minibuffer-flag t)
   (set-face-foreground 'aw-background-face "gray70")
   (ace-window-display-mode)
-  :init
-  (add-hook 'term-mode-hook
-            (lambda ()
-              (define-key term-raw-map (kbd "M-o") 'ace-window)))
+  :init (add-hook 'term-mode-hook
+                  (lambda ()
+                    (define-key term-raw-map (kbd "M-o") 'ace-window)))
   :bind (( "M-o" . ace-window)))
 
 ;; Window and buffer management
@@ -76,6 +67,13 @@
          ("C-c v l" . avy-goto-line)
          ("C-c v w" . avy-goto-word-1)))
 
+(defun lk/kill-dired-buffers ()
+  (interactive)
+  (mapc
+   (lambda (buffer)
+     (when (eq 'dired-mode (buffer-local-value 'major-mode buffer))
+       (kill-buffer buffer)))
+   (buffer-list)))
 
 (use-package emamux)
 
