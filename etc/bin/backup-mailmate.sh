a41010438e513eeb58ba4bad74b5d117b5c6b01e
Łukasz Korecki <lukasz@korecki.me>
2024-03-17 10:06:22 -0700

    reformat all scripts

--- etc/bin/backup-mailmate.sh
+++ etc/bin/backup-mailmate.sh
@@ -4,24 +4,24 @@ backupPath=~/Dropbox/AppBackup/Mailmate
 mkdir -p "$backupPath"
 
 backup() {
-    echo 'baccking up'
-    cp -rv	~/Library/Application\ Support/MailMate/*.plist "${backupPath}/"
-    cp -rv ~/Library/Preferences/com.freron.MailMate.plist "${backupPath}/"
+  echo 'baccking up'
+  cp -rv ~/Library/Application\ Support/MailMate/*.plist "${backupPath}/"
+  cp -rv ~/Library/Preferences/com.freron.MailMate.plist "${backupPath}/"
 
 }
 restore() {
-    echo 'restoring'
-    cp -rv "${backupPath}/*.plist" ~/Library/Application\ Support/MailMate/
-    cp -rv "${backupPath}/com.freron.MailMate.plist" ~/Library/Preferences/com.freron.MailMate.plist
+  echo 'restoring'
+  cp -rv "${backupPath}/*.plist" ~/Library/Application\ Support/MailMate/
+  cp -rv "${backupPath}/com.freron.MailMate.plist" ~/Library/Preferences/com.freron.MailMate.plist
 }
 
 cmd=${1:-backup}
 
-if [[ "$cmd" = "backup" ]] ; then
-    backup
-elif [[ "$cmd" == "restore" ]] ; then
-    restore
+if [[ $cmd == "backup" ]]; then
+  backup
+elif [[ $cmd == "restore" ]]; then
+  restore
 else
-    echo "unknown command!: $cmd"
-    exit 1
+  echo "unknown command!: $cmd"
+  exit 1
 fi
