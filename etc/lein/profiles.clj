8a693fd56e1a27b7f2ff42375aad3d1660378ec7
Łukasz Korecki <lukasz@korecki.me>
2024-04-09 08:29:56 -0700

    expand antq aliases in lein profile

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -3,7 +3,9 @@
  :user {:dependencies [^:displace [org.clojure/clojure "1.11.1"]
                        [org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
         :aliases {"kaocha" ["run" "-m" "kaocha.runner"]
-                  "outdated" ["with-profile" "antq" "run" "-m" "antq.core"]}}
+                  "outdated-check" ["with-profile" "+antq,+dev" "run" "-m" "antq.core"]
+                  "outdated" ["with-profile" "+antq" "+dev" "run" "-m" "antq.core" "--upgrade" "--force"]}}
+
  :antq {:dependencies [[com.github.liquidz/antq "RELEASE"]
                        [org.yaml/snakeyaml "2.2"]
                        [org.slf4j/slf4j-nop "RELEASE"]]}}
