163f284d423aef3d176c47ba7874864361ef5c7b
Łukasz Korecki <lukasz@korecki.me>
2024-07-19 09:17:20 -0700

    Add cloverage lein task

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,14 +1,22 @@
-{:repl {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
-        :repl-options {:color false}}
- :user {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
+{:user {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
 
-        :aliases {"kaocha" ["run" "-m" "kaocha.runner"]
+        :aliases {"kaocha" ["run" "-m"
+                            "kaocha.runner"]
                   "outdated-check" ["with-profile" "antq" "run" "-m" "antq.core"]
-                  "outdated" ["with-profile" "antq" "run" "-m" "antq.core" "--upgrade" "--force"]}}
+                  "outdated" ["with-profile" "antq"
+                              "run" "-m" "antq.core" "--upgrade" "--force"]
+
+                  "cloverage" ["with-profile" "+coverage"
+                               "cloverage" "-e" ".*dev.*" "-e" ".*repl.*" "-e" ".*benchmark.*"]}}
+
+ :repl {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
+        :repl-options {:color false}}
 
  :antq {:dependencies [[com.github.liquidz/antq "RELEASE"]
                        [org.clojure/tools.reader "1.4.2"]
                        [org.yaml/snakeyaml "2.2"]
                        ^:displace [org.slf4j/slf4j-api "1.7.14"]
 
-                       [org.slf4j/slf4j-nop "RELEASE"]]}}
+                       [org.slf4j/slf4j-nop "RELEASE"]]}
+
+ :coverage {:plugins [[lein-cloverage "1.2.2"]]}}
