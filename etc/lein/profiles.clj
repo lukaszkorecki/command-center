de9504de9120d5682e010249ba8e8801fbdd1573
Łukasz Korecki <lukasz@korecki.me>
2024-08-14 08:30:16 -0700

    Clean up lein profile

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
 
-        :aliases {"kaocha" ["run" "-m"
-                            "kaocha.runner"]
+        :aliases {"kaocha" ["run" "-m" "kaocha.runner"]
                   "outdated-check" ["with-profile" "antq" "run" "-m" "antq.core"]
                   "outdated" ["with-profile" "antq"
                               "run" "-m" "antq.core" "--upgrade" "--force"]
@@ -13,10 +12,7 @@
         :repl-options {:color false}}
 
  :antq {:dependencies [[com.github.liquidz/antq "RELEASE"]
-                       [org.clojure/tools.reader "1.4.2"]
-                       [org.yaml/snakeyaml "2.2"]
-                       ^:displace [org.slf4j/slf4j-api "1.7.14"]
-
+                       ^:displace [org.slf4j/slf4j-api "RELEASE"]
                        [org.slf4j/slf4j-nop "RELEASE"]]}
 
  :coverage {:plugins [[lein-cloverage "1.2.2"]]}}
