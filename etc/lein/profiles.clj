a8627ad4a5e5e4135d704caf48905cc47b1745c6
Łukasz Korecki <lukasz@korecki.me>
2024-08-14 10:19:05 -0700

    Lock down dependencies in antq profile

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -1,18 +1,27 @@
 {:user {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
 
-        :aliases {"kaocha" ["run" "-m" "kaocha.runner"]
-                  "outdated-check" ["with-profile" "antq" "run" "-m" "antq.core"]
+        :aliases {;; do not override `lein test`
+                  "kaocha" ["run" "-m" "kaocha.runner"]
+
+                  "outdated-check" ["with-profile" "antq"
+                                    "run" "-m" "antq.core"]
+
                   "outdated" ["with-profile" "antq"
                               "run" "-m" "antq.core" "--upgrade" "--force"]
 
                   "cloverage" ["with-profile" "+coverage"
-                               "cloverage" "-e" ".*dev.*" "-e" ".*repl.*" "-e" ".*benchmark.*"]}}
+                               "cloverage"
+                               "-e" ".*dev.*"
+                               "-e" ".*repl.*"
+                               "-e" ".*benchmark.*"]}}
 
  :repl {:dependencies [[org.clojars.lukaszkorecki/rumble "1.0.0.24"]]
         :repl-options {:color false}}
 
- :antq {:dependencies [[com.github.liquidz/antq "RELEASE"]
+ :antq {:global-vars {*warn-on-reflection* false}
+        :dependencies [[com.github.liquidz/antq "2.7.1133"]
+                       ;; silencelogs from antq
                        ^:displace [org.slf4j/slf4j-api "RELEASE"]
-                       [org.slf4j/slf4j-nop "RELEASE"]]}
+                       [org.slf4j/slf4j-nop "2.0.9"]]}
 
  :coverage {:plugins [[lein-cloverage "1.2.2"]]}}
