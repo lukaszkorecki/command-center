85b5a2854c96f706cb5df534c616b2da0ba25b17
Łukasz Korecki <lukasz@korecki.me>
2024-08-15 09:21:03 -0700

    Sort out antq blowing up in some projects where lein deps get mixed up

--- etc/lein/profiles.clj
+++ etc/lein/profiles.clj
@@ -3,7 +3,7 @@
         :aliases {;; do not override `lein test`
                   "kaocha" ["run" "-m" "kaocha.runner"]
 
-                  "outdated-check" ["with-profile" "antq"
+                  "outdated-check" ["with-profile" "+antq,-dev,-repl,-cloverage,-system,-base,-user"
                                     "run" "-m" "antq.core"]
 
                   "outdated" ["with-profile" "antq"
@@ -19,9 +19,13 @@
         :repl-options {:color false}}
 
  :antq {:global-vars {*warn-on-reflection* false}
-        :dependencies [[com.github.liquidz/antq "2.7.1133"]
+        :dependencies [[com.github.liquidz/antq "RELEASE"]
                        ;; silencelogs from antq
                        ^:displace [org.slf4j/slf4j-api "RELEASE"]
-                       [org.slf4j/slf4j-nop "2.0.9"]]}
+                       [org.slf4j/slf4j-nop "2.0.9"]
 
+                       ;; these are needed when running checks
+                       ;; in repos with older dependencies
+                       [org.clojure/tools.reader "1.4.2"]
+                       [org.yaml/snakeyaml "2.2"]]}
  :coverage {:plugins [[lein-cloverage "1.2.2"]]}}
