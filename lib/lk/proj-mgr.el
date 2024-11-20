2c3347831e90a9452e83e31f8c8b24e4758d8970
Łukasz Korecki <lukasz@korecki.me>
2024-11-21 00:41:13 +0100

    Clean up clean up clena up

--- lib/lk/proj-mgr.el
+++ lib/lk/proj-mgr.el
@@ -13,24 +13,28 @@
 (require 'transient)
 (require 'dash)
 
-(defun hput (h k v) (puthash k v h))
+(defun hm--put (h k v) (puthash k v h))
 
-(defun hget (h k) (gethash k h))
+(defun hm--get (h k) (gethash k h))
 
 
-(defvar lk/project-file-associations
+(defvar pjmgr--project-file-associations
   (let ((pj-file->type (make-hash-table :test 'equal)))
-    (hput pj-file->type "deps.edn" 'clojure)
-    (hput pj-file->type "project.clj" 'clojure)
+    (hm--put pj-file->type "deps.edn" 'clojure)
+    (hm--put pj-file->type "project.clj" 'clojure)
     ;; TODO: bb.edn, main.tf, package.json, Gemfile, possibly more?
     ;; Add more associations as needed
     pj-file->type))
 
-(defun lk/loc-dom-file->name (dir name)
+(defun pjmgr--loc-dom-file->name (dir name)
   (when (locate-dominating-file dir name) name))
 
+(defun pjmgr--project-name-or-nil ()
+  (when-let ((pj (project-current)))
+    (project-name pj)))
 
-(defun lk/get-root-file-and-project-type ()
+
+(defun pjmgr--get-project-info-maybe ()
   (let* ((pj-info (make-hash-table :test 'equal))
          (pj-root
           (condition-case root-file-err
@@ -40,129 +44,136 @@
              default-directory)))
          (pj-file
           (or
-           (lk/loc-dom-file->name default-directory "deps.edn")
-           (lk/loc-dom-file->name default-directory "project.clj")))
+           (pjmgr--loc-dom-file->name default-directory "deps.edn")
+           (pjmgr--loc-dom-file->name default-directory "project.clj")))
 
          (pj-type
           (when pj-file
-            (gethash pj-file lk/project-file-associations nil)))
+            (gethash pj-file pjmgr--project-file-associations nil)))
 
          (pj-clj-nrepl-running?
           (and
            (equal 'clojure pj-type)
            (locate-dominating-file default-directory ".nrepl-port"))))
 
-    (hput pj-info "name"
-          (when pj-file (project-name (project-current))))
-    (hput pj-info "root" pj-root)
-    (hput pj-info "cwd" default-directory)
-    (hput pj-info "type" pj-type)
-    (hput pj-info "project-file" pj-file)
-    (hput pj-info "repo" (vc-root-dir))
-    (hput pj-info "clj-nrepl-running?" pj-clj-nrepl-running?)
+    (hm--put pj-info "name" (pjmgr--project-name-or-nil))
+    (hm--put pj-info "root" pj-root)
+    (hm--put pj-info "cwd" default-directory)
+    (hm--put pj-info "type" pj-type)
+    (hm--put pj-info "project-file" pj-file)
+    (hm--put pj-info "repo" (vc-root-dir))
+    (hm--put pj-info "clj-nrepl-running?" pj-clj-nrepl-running?)
     pj-info))
 
 
 ;; -----
 
-(defun lk/print-project-info ()
-  (let* ((pj-info (lk/get-root-file-and-project-type))
-         (pj-type (hget pj-info "type"))
-         (pj-cwd (hget pj-info "cwd"))
-         (pj-root (hget pj-info "root"))
-         (is-repo? (hget pj-info "repo")))
+(defun pjmgr--overview-str (pj-name pj-info)
+  (let* ((pj-type (hm--get pj-info "type"))
+         (pj-root (hm--get pj-info "root")))
     (if pj-type
-        (format  "%s [%s->%s]"
-                 pj-cwd
-                 (hget pj-info "type")
-                 (hget pj-info "project-file"))
-      (format "in %s" (hget pj-info "root")))))
+        (format  "Project: %s [%s] (%s)" pj-name pj-type pj-root)
+      (format "In %s" pj-root))))
 
 
 ;; XXX: I wonder if I can plug in magit's own suffixes/groups here?
-(defun lk/print-project-git-branch ()
+(defun pjmgr--git-info ()
   (if-let ((branch (vc-status-mode-line)))
       (format "Branch: %s" (s-replace "Git-" "" (first branch)))
     "Not a git repo"))
 
 
-(defun lk/mk-sffx (a-list)
+(defun pjmgr--mk-suffix (a-list)
   "Util for creating suffixes for Transient"
   (transient-parse-suffix transient--prefix a-list))
 
+(defun pjmgr--list->suffixes (a-list)
+  (->> a-list -non-nil (mapcar #'pjmgr--mk-suffix)))
+
+
+(defun pjmgr--start-group (_)
+  (let* ((pj-name (pjmgr--project-name-or-nil))
+         (pj-info (when pj-name (pjmgr--get-project-info-maybe)))
+
+         (pj-overview
+          (when pj-name (pjmgr--overview-str pj-name pj-info)))
+
+         (is-git-repo? (when pj-info (hm--get pj-info "repo"))))
+
+    (if pj-name
+        (mapcar #'pjmgr--mk-suffix
+                (-non-nil
+                 (list
+                  (when pj-info '(:info pj-overview))
+                  (when is-git-repo? '(:info #'pjmgr--git-info)))))
+
+      ;; else
+      (list (pjmgr--mk-suffix '(:info "<not in a project>"))))))
+
+
+(defun pjmgr--actions-group (_)
+  (let* ((pj-info (pjmgr--get-project-info-maybe))
+         (is-git-repo? (hm--get pj-info "repo")))
+
+    (mapcar #'pjmgr--mk-suffix
+            (-non-nil
+             (list
+              '("p" "select a different project" project-switch-project)
+
+              '("t"  "start vterm"  multi-vterm-project)
+
+              (when is-git-repo? '("s" "magit status" magit-status))
+
+              (when is-git-repo? '("g" "git grep" counsel-git-grep)))))))
+
+
+(defun pjmgr--clojure-cmds-group (pj-info)
+  (let* ((pj-clj-nrepl-running?
+          (when pj-is-clojure? (hm--get pj-info "clj-nrepl-running?"))))
+
+    (if pj-clj-nrepl-running?
+        (mapcar #'pjmgr--mk-suffix
+                (list
+
+                 '("m" :info
+                   (format "nREPL server is running: %s"
+                           (monroe-locate-running-nrepl-host)))
+
+                 '("r" "Switch to the REPL buffer" lk/switch-to-monroe-repl-or-connect-or-start)
+
+                 '("s" "Jump to scratch file" lk/clojure-scratch)
+
+                 '("K"  "Kill monroe server & REPL buffer" lk/monroe-kill-all)))
+
+      ;; we can only start
+      (mapcar #'pjmgr--mk-suffix
+              (list
+               '(:info "nREPL server not running")
+               '("m" "start Monroe & nREPL server" lk/switch-to-monroe-repl-or-connect-or-start))))))
+
+
+(defun pjmgr--cmds-group (_)
+  (let* ((pj-info (pjmgr--get-project-info-maybe))
+         ;; TODO: this eventually will be a cond-powered dispatch
+         (pj-is-clojure?
+          (and pj-info (equal 'clojure (hm--get pj-info "type")))))
+    (if pj-is-clojure? (pjmgr--clojure-cmds-group pj-info)
+
+      '())))
+
 ;; main transient config
-(transient-define-prefix lk/proj-management
+(transient-define-prefix lk/proj-mgr
   ()
 
   "Manages current project, and shows its info"
-  [[:description
-    (lambda ()
-      (if-let ((name (hget (lk/get-root-file-and-project-type) "name")))
-          (format "Project: %s" name)
-        "<not in a project>"))
-
-    (:info #'lk/print-project-info)
-    (:info #'lk/print-project-git-branch)
-    ""
-    ]
-
+  [["🌊 Start"
+    :setup-children pjmgr--start-group]
 
    ["Actions" ;; dispatch generic commands
-    :setup-children (lambda
-                      (_)
-                      (let* ((pj-info (lk/get-root-file-and-project-type))
-                             (is-git-repo? (hget pj-info "repo")))
+    :setup-children pjmgr--actions-group]]
 
-                        (-non-nil
-                         (list
-                          (lk/mk-sffx
-                           '("t" "start vterm in project root" multi-vterm-project))
+  ["Commands" :setup-children pjmgr--cmds-group])
 
-                          (when is-git-repo?
-                            (lk/mk-sffx '("s" "magit status" magit-status)))
-
-                          (when is-git-repo?
-                            (lk/mk-sffx '("g" "git grep" counsel-git-grep)))))))
-    ]]
-
-  ;; TODO: re-organize this to be dynamic based on the project type, Clj only for now!
-  ;; use :setup-children (lambda (_) (list ....
-  ;; to dynamically generate language specific commands
-
-  ["Commands"
-   :setup-children (lambda
-                     (_)
-                     (let* ((pj-info (lk/get-root-file-and-project-type))
-                            (pj-is-clojure? (equal 'clojure (hget pj-info "type")))
-                            (pj-clj-nrepl-running?
-                             (when pj-is-clojure? (hget pj-info "clj-nrepl-running?"))))
-
-                       (if pj-is-clojure?
-                           (if pj-clj-nrepl-running?
-                               (list
-                                (lk/mk-sffx
-                                 '("m" :info
-                                   (format "nREPL server already: %s"
-                                           (monroe-locate-running-nrepl-host))))
-
-                                (lk/mk-sffx
-                                 '("r" "Switch to the REPL buffer" lk/switch-to-monroe-repl-or-connect-or-start))
-
-                                (lk/mk-sffx
-                                 '("s" "Jump to scratch file" lk/clojure-scratch))
-
-                                (lk/mk-sffx
-                                 '("K"  "Kill monroe server & REPL buffer" lk/monroe-kill-all)))
-
-                             ;; we can only start
-                             (list
-                              (lk/mk-sffx
-                               '("m" "start Monroe & nREPL server" lk/switch-to-monroe-repl-or-connect-or-start))))
-                         ;; not a Clj project, do nothing (for now!)
-                         '())))
-   ;; end Commands here
-   ])
-
-(define-key global-map (kbd "C-c d") 'lk/proj-management)
+(define-key global-map (kbd "C-c d") 'lk/proj-mgr)
 
 (provide 'lk/proj-mgr)
