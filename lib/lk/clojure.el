c92a771055ddd7f9fe6a2d1810be027b7a42a538
Łukasz Korecki <lukasz@korecki.me>
2025-05-19 07:24:06 -0700

    monroe + custom code -> cider

--- lib/lk/clojure.el
+++ lib/lk/clojure.el
@@ -1,146 +1,62 @@
-; -*- lexical-binding: t; -*-
+;; -*- lexical-binding: t; -*-
 ;;; clojure.el --- clojure setup
 ;;; Commentary:
 ;; inf-clojure/monroe based clj-scratch buffer
 ;; Adopted from cider's scratch
 ;;; Code:
 
-
-(use-package clojure-mode
-  ;; :straight (:host github :repo "clojure-emacs/clojure-mode")
-  :init (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-  (add-hook 'clojure-mode-hook #'lk/clj-mode-hook)
-
-  ;;  (setq clojure-indent-style 'align-arguments)
-  (add-hook 'clojure-mode-hook #'aggressive-indent-mode))
-
-
-(defconst lk/clj-scratch-name "scratch.clj")
-
-(defun lk/init-clojure-scratch ()
-  (interactive)
-  (let* ((project-root (lk/project-find-root nil))
-         (scratch-file
-          (format "%s%s" project-root lk/clj-scratch-name)))
-    (find-file scratch-file)
-    (get-buffer lk/clj-scratch-name)))
-
-
-(defun lk/clojure-scratch ()
-  (interactive)
-  (pop-to-buffer
-   (or (get-buffer lk/clj-scratch-name) (lk/init-clojure-scratch))
-   'display-buffer-reuse-window))
-
-
-;; (use-package clojure-mode-extra-font-locking)
-
-
-(defun lk/failed-tests-in-monroe-repl ()
+;; helpers
+(defun lk/failed-tests-in-repl-buffer ()
   (interactive)
   (consult-line "\\(FAIL\\|ERROR\\).in."))
 
-(defun lk/switch-to-monroe-repl-or-connect-or-start ()
+
+(defun lk/cider-kill-all ()
   (interactive)
-  (if (get-buffer (format "*%s*" monroe-nrepl-server-buffer-name ))
-      (let* ((nrepl-addr (monroe-locate-running-nrepl-host)))
-        (if nrepl-addr
-            (if-let (b (monroe-repl-buffer))
-                ;; switch to repl
-                (pop-to-buffer b)
-              ;; we need to connect - server is possibly running
-              (monroe nrepl-addr))
-          ;; else
-          (error
-           (format ".nrepl-port file not found in %s" default-directory))))
-    ;; else
-    (and
-     (message "Monroe server buffer not found, gonna start it and you need to try again")
-     (monroe-nrepl-server-start))))
-
-(use-package monroe
-  :init (require 'monroe)
-  :config (setq monroe-nrepl-server-cmd "start-clojure-repl-process")
-  ;; optimize memory usage for the REPL buffer, we don't really need undo here
-  (add-hook 'monroe-mode-hook
-            (lambda () (setq-local undo-outer-limit 10000)))
-  :bind (:map clojure-mode-map
-              ("C-c C-c" . monroe-eval-expression-at-point)
-              ("C-x c j" . lk/switch-to-monroe-repl-or-connect-or-start)
-              ("C-x c m" . lk/switch-to-monroe-repl-or-connect-or-start)
-              ("C-c C-z" . lk/switch-to-monroe-repl-or-connect-or-start)
-              ("C-c C-l" . monroe-load-file)
-              ("C-x c l" . lk/init-clojure-scratch)
-              ("C-x c s " . lk/clojure-scratch ))
-  (:map monroe-mode-map
-        (("C-c n i " . lk/failed-tests-in-monroe-repl)
-         ("C-x c s " . lk/clojure-scratch ))))
+  (kill-matching-buffers ".*nrepl.*" t t)
+  (kill-matching-buffers ".*cider.*" t t))
 
 
-(defun lk/monroe-kill-all ()
+(defun lk/portal-open ()
   (interactive)
-  (kill-matching-buffers ".*monroe.*" t t)
-  (kill-matching-buffers "webkit.*monroe" t t))
+  (cider-nrepl-sync-request:eval
+   "(do (require 'r 'r.portal 'r.webview) (r.webview/browse (r.portal/start! {:launcher false :browse? false})))"))
 
-
-(defun lk/clj-mode-hook ()
-  (require 'monroe)
-  (clojure-enable-monroe))
-
-(defun clear-comint-buffer-by-match (buffer-regexp)
-  (let* ((repl-buffers
-          (cl-remove-if-not
-           (lambda (buffer)
-             (string-match-p buffer-regexp (buffer-name buffer)))
-           (buffer-list))))
-    (dolist (repl-buffer repl-buffers)
-      (with-current-buffer repl-buffer (comint-clear-buffer)))))
-
-(defun lk/clear-monroe-repl-from-anywhere ()
-  "Clear the NREPL server buffer."
+(defun lk/portal-clear ()
   (interactive)
-  (clear-comint-buffer-by-match  "\\*monroe: localhost:.*\\*"))
+  (cider-nrepl-sync-request:eval "(r.portal/clear!)"))
 
-
-(defun lk/clear-monroe-server-buffer-from-anywhere ()
-  "Clear the Clojure REPL buffer."
+(defun lk/portal-close ()
   (interactive)
-  (clear-comint-buffer-by-match  ".*monroe nrepl server.*"))
+  (cider-nrepl-sync-request:eval "(r.portal/close!)"))
 
-(defun lk/monroe-eval-code-and-callback-with-value (code-str on-value)
-  (monroe-send-eval-string
-   code-str
-   (lambda (response)
-     (condition-case err
-         (monroe-dbind-response response
-                                (value status id)
-                                (when value (funcall on-value value))
-                                (when (member "done" status)
-                                  (remhash id monroe-requests)))
-       (error (message "Eval error %s" err))))))
+;; packages
+
+(use-package clojure-mode
+  ;; :straight (:host github :repo "clojure-emacs/clojure-mode")
+  :init ;
+  (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+  (add-hook 'clojure-mode-hook #'aggressive-indent-mode))
 
 
-(defun lk/monroe-portal-start! ()
-  (interactive)
-  (lk/monroe-eval-code-and-callback-with-value
-   "(do
-      (require 'r 'r.portal)
-      (r.portal/start! {:browse? true :launcher false}))"
-   (lambda (value)
-     (condition-case err
-         ;; value is a raw string, so we need to remove " from it
-         (let ((url (string-replace "\"" "" value)))
-           (message "Opening portal %s" url)
-           (condition-case err2
-               (xwidget-webkit-browse-url url)
-             (error (message "Browse error %s" err2))))
-       (error (message "Portal start error %s" err))))))
+(use-package cider
+  :ensure t
+  :after (clojure-mode)
+  :init ;
+  (setq cider-enable-nrepl-jvmti-agent t)
+  :bind (("C-x s" . cider-scratch)
+         ("C-x c j" . cider-jack-in)
+         ("C-x p o" . lk/portal-open)
+         ("C-x p c" . lk/portal-clear)
+         ("C-x p q" . lk/portal-close)
+         ("C-c C-d" . cider-doc)
+         ("C-c C-e" . cider-eval-last-sexp)
+         ("C-c C-r" . cider-eval-region)
+         ("C-c C-b" . cider-eval-buffer)
+         ("C-c C-z" . cider-switch-to-repl-buffer)))
 
-(defun lk/portal-clear! ()
-  (interactive)
-  (lk/monroe-eval-code-and-callback-with-value
-   "(r.portal/clear!)"
-   (lambda (value) (message "Portal cleared"))))
+(use-package kaocha-runner
+  :after (cider-mode))
 
 (provide 'lk/clojure)
 ;;; clojure.el ends here
