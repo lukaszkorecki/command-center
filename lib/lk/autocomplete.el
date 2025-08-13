79dbd1206bd6ac34e154e60b17465d03c14fccff
Łukasz Korecki <lukasz@korecki.me>
2025-08-13 12:30:09 -0700

    Update eca-emacs to bring the built-in transient menu

--- lib/lk/autocomplete.el
+++ lib/lk/autocomplete.el
@@ -85,25 +85,11 @@
 
 
 (use-package eca
-  :straight (:host github :repo "editor-code-assistant/eca-emacs" :files
-                   ("*.el"))
+  :straight (:host github :repo "editor-code-assistant/eca-emacs" :files ("*.el"))
   :ensure t
   :init ;
-  (transient-define-prefix lk/eca
-    ()
-    "ECA commands."
-    [["Chat"
-      ("c" "Clear" eca-chat-clear)
-      ("r" "Reset" eca-chat-reset)]
-     ["Other"
-      ("m" "MCP details" eca-mcp-details)
-      ("E" "Show stderr" eca-show-stderr)
-      ("R" "Stop" eca-restart)
-      ("S" "Stop" eca-stop)]])
   (setq eca-chat-custom-model "gemini-2.5-pro")
-  (setq eca-extra-args '("--verbose" "--log-level" "debug"))
-
-  :bind (:map eca-chat-mode-map (("C-c y" . lk/eca))))
+  (setq eca-extra-args '("--verbose" "--log-level" "debug")))
 
 (provide 'lk/autocomplete)
 
