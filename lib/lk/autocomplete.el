e6716d08d6c495fa0f9193cee9bb1b9b2e70f0e8
Łukasz Korecki <lukasz@korecki.me>
2025-10-11 10:37:49 -0700

    maybe fix company mode?

--- lib/lk/autocomplete.el
+++ lib/lk/autocomplete.el
@@ -13,6 +13,7 @@
   ;; invert the navigation direction if the the completion popup-isearch-match
   ;; is displayed on top (happens near the bottom of windows)
   (setq company-tooltip-flip-when-above t)
+  :init
   (global-company-mode)
   :bind (( "C-c M-c" . company-complete)
          :map company-active-map
@@ -27,7 +28,7 @@
 
 (use-package company-box
   :ensure t
-  :after company
+  :after (company)
   :hook (company-mode . company-box-mode)
   :init (company-box-mode))
 
