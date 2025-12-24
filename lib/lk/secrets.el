d1054338cc14e5e6acb8ea07fd11c2455665e572
Łukasz Korecki <lukasz@korecki.me>
2025-12-24 09:29:07 -0800

    Fix secret loading once more

--- lib/lk/secrets.el
+++ lib/lk/secrets.el
@@ -2,28 +2,33 @@
 
 (use-package load-env-vars :ensure t)
 
-(defun lk/load-secrets-from-1p ()
+(defun lk/load-secrets-from-1p (control-var env-tmpl-path)
   (interactive)
   (require 'load-env-vars)
-  (if (not (getenv "OP_SECRETS_LOADED"))
-      (condition-case err
+  (if (file-exists-p env-tmpl-path)
+      (progn
+        (if (not (getenv control-var))
+            (condition-case err
+                (let* ((tmp-env-file-path
+                        (make-temp-file "emacs-secrets" nil ".env")))
+                  (shell-command-to-string
+                   (format "op inject -i  -o %s"
+                           (expand-file-name env-tmpl-path tmp-env-file-path)))
+                  (load-env-vars tmp-env-file-path)
+                  (delete-file tmp-env-file-path)
+                  (setenv control-var "true")
+                  (message "loaded secrets from 1password via %s" env-tmpl-path))
+              (error (message "Error loading secrets: %s" err)))
           (progn
-            (shell-command-to-string "~/.emacs.d/etc/bin/op-secret-loader -c ~/.private/secrets.edn -f env > /tmp/emacs.env")
-            (load-env-vars "/tmp/emacs.env")
-            (delete-file "/tmp/emacs.env")
-            (message "loaded secrets from 1password"))
-        (error (message "Error loading secrets: %s" err)))
-    (progn
-      (setenv "OP_SECRETS_LOADED" "true")
-      (message "already loaded"))))
+            (setenv "OP_SECRETS_LOADED" "true")
+            (message "already loaded"))))
 
+    (message "ERROR: env template file doesn't exist: %s" env-tmpl-path)))
+
+(lk/load-secrets-from-1p "OP_SECRETS_LOADED" "~/.private/secrets.env.tmpl")
 
 (when (file-exists-p "~/.private/work-secrets.el")
+  (message "loading custom work secrets")
   (load "~/.private/work-secrets.el"))
-
-(when (fboundp 'lk/load-work-secrets-from-1p)
-  (lk/load-work-secrets-from-1p))
-
-(lk/load-secrets-from-1p)
 
 (provide 'lk/secrets)
