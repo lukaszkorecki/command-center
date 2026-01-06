e29b57d7703b3ea51da1f4a5247d1b6b5856f551
Łukasz Korecki <lukasz@korecki.me>
2026-01-06 14:43:01 -0800

    Unify formatter and indent settings for clojure

--- custom.el
+++ custom.el
@@ -9,21 +9,17 @@
      default))
  '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
  '(package-selected-packages
-   '(ace-window agent-shell aggressive-indent alabaster-theme
-                better-defaults copilot csv-mode difftastic disproject
+   '(ace-window agent-shell better-defaults copilot difftastic disproject
                 dockerfile-mode eca edit-indirect elfmt embark-consult
-                espresso-theme exec-path-from-shell git git-link
-                go-mode graphql-mode hl-todo ibuffer-project
-                jinja2-mode js-jsx-mode js3-mode json-mode
+                exec-path-from-shell git git-link go-mode graphql-mode
+                hl-todo ibuffer-project jinja2-mode json-mode
                 kaocha-runner keychain-environment load-env-vars
-                logview lua-mode marginalia markdown-ts-mode
-                mermaid-mode mise modus-themes multi-vterm
-                multiple-cursors nginx-mode nord-theme orderless
-                project-rootfile python-mode restclient sqlup-mode
-                string-inflection swift-mode terraform-mode time-zones
-                transpose-frame undo-tree unicode-fonts vertico
-                visual-regexp vterm-anti-flicker-filter web-mode
-                yaml-mode yasnippet))
+                marginalia mermaid-mode mise modus-themes multi-vterm
+                multiple-cursors nginx-mode orderless project-rootfile
+                python-mode restclient sqlup-mode string-inflection
+                swift-mode terraform-mode time-zones transpose-frame
+                undo-tree unicode-fonts vertico visual-regexp
+                vterm-anti-flicker-filter web-mode yaml-mode yasnippet))
  '(package-vc-selected-packages
    '((load-env-vars :url
                     "https://github.com/lukaszkorecki/emacs-load-env-vars")
