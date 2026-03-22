37faf137fb411cdae772968f364d8f02f589db44
Łukasz Korecki <lukasz@korecki.me>
2026-03-22 09:58:30 -0500

    rework secret loading

--- lib/lk/secrets.el
+++ lib/lk/secrets.el
@@ -3,26 +3,26 @@
 (use-package load-env-vars
   :vc (:url "https://github.com/lukaszkorecki/emacs-load-env-vars"
             :rev "ccdd7cd9a0abb79c9d3b327b792e192bb87e2eba")
-  :ensure t)
+  :ensure t
+  :config ;; do this at init/config stage to avoid issues with load-env-vars not working
+  (defun lk/load-secrets-from-1p
+              (env-tmpl-path)
+            (require 'load-env-vars)
+            (load-env-vars-from-string
+             (shell-command-to-string
+              (format "/opt/homebrew/bin/op --account my.1password.com inject -i %s"
+                      (expand-file-name env-tmpl-path)))))
 
-(defun lk/load-secrets-from-1p (env-tmpl-path)
-  (require 'load-env-vars)
-  (load-env-vars-from-string
-   (shell-command-to-string
-    (format "/opt/homebrew/bin/op --account my.1password.com inject -i %s" (expand-file-name env-tmpl-path)))))
+  (let ((personal-secrets-loader-el
+         (expand-file-name "~/.emacs.d/private-configs/secrets/personal-secrets.el")))
+    (when (file-exists-p personal-secrets-loader-el)
+      (message "loading custom personal secrets")
+      (load personal-secrets-loader-el)))
 
-(let ((personal-secrets-loader-el
-       (expand-file-name "~/.emacs.d/private-configs/secrets/personal-secrets.el")))
-  (when (file-exists-p personal-secrets-loader-el)
-    (message "loading custom personal secrets")
-    (load personal-secrets-loader-el)))
-
-
-
-(let ((work-secrets-loader-el
-       (expand-file-name "~/.emacs.d/private-configs/secrets/work-secrets.el")))
-  (when (file-exists-p work-secrets-loader-el)
-    (message "loading custom work secrets")
-    (load work-secrets-loader-el)))
+  (let ((work-secrets-loader-el
+         (expand-file-name "~/.emacs.d/private-configs/secrets/work-secrets.el")))
+    (when (file-exists-p work-secrets-loader-el)
+      (message "loading custom work secrets")
+      (load work-secrets-loader-el))))
 
 (provide 'lk/secrets)
