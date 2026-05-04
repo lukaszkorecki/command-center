da4a62a246533f48e7339b8f8315bebd7f1fa35e
Łukasz Korecki <lukasz@korecki.me>
2026-05-04 13:15:13 -0700

    clean up custom.el

--- custom.el
+++ custom.el
@@ -4,14 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c23446c6dd105f7eda80655f500e8554d701c7729867a1ad578c211b47a25482"
-     default))
- '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((load-env-vars :url
-                    "https://github.com/lukaszkorecki/emacs-load-env-vars"))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
