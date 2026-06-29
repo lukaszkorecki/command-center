654de7795020e961d34b296ed2c94d1078d59c28
Łukasz Korecki <lukasz@korecki.me>
2026-06-29 07:37:40 -0700

    really pin emacs 31

--- Brewfile
+++ Brewfile
@@ -1,7 +1,7 @@
 # -*- mode: ruby -*-
 tap 'd12frosted/emacs-plus'
 cask 'cleanshot'
-cask 'emacs-plus@31'
+cask 'emacs-plus-app'
 cask 'alfred'
 cask 'mailmate@beta'
 cask 'ghostty'
--- custom.el
+++ custom.el
@@ -1,14 +1,1 @@
 ;;; -*- lexical-binding: t -*-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dashy ghostel))
- '(package-vc-selected-packages '((dashy :url "https://github.com/lukaszkorecki/dashy"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
